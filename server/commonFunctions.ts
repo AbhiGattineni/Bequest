f287bbc2f78278f419ddbe8389010d8e7beda5b2
AbhiGattineni <abhishekgattineni@gmail.com>
2024-01-05 03:40:23 -0500

    creation of separate backup file and saving data in encrypted form implemented

--- server/commonFunctions.ts
+++ server/commonFunctions.ts
@@ -24,8 +24,38 @@ function performBackup() {
 
     try {
         const data = fs.readFileSync(dataFilePath, 'utf8');
+        //backing up the data to the backup file
         fs.appendFileSync(backupDir, data, 'utf8');
-        console.log('Backup successful:', backupDir);
+
+        //creating a new backup file with current timestamp
+        const filePath = `../backups/backup-${getCurrentNumericDate()}.log`; 
+        fs.writeFile(filePath,data , (err) => {
+            if (err) {
+                console.error('There was an error writing to the file:', err);
+            } else {
+                console.log('File was written successfully');
+            }
+        });
+
+const filePathEncrypted = `../encryptedBackup/backup-${getCurrentNumericDate()}.log`; 
+
+const algorithm = 'aes-256-cbc';
+const secretKey = 'vOVH6sdmpNWjRRIqCc7rdxs01lwHzfr3'; 
+const iv = crypto.randomBytes(16);
+
+function encrypt(text:any) {
+    const cipher = crypto.createCipheriv(algorithm, secretKey, iv);
+    const encrypted = Buffer.concat([cipher.update(text), cipher.final()]);
+    return { iv: iv.toString('hex'), content: encrypted.toString('hex') };
+  }
+        fs.writeFile(filePathEncrypted,JSON.stringify(encrypt(data)) , (err) => {
+            if (err) {
+                console.error('There was an error writing to the encrypted file:', err);
+            } else {
+                console.log('Encrypted file was written successfully');
+            }
+        });
+
     } catch (error: any) {
         console.error('Error during backup:', error.message);
         console.error('Error code:', error.code);
@@ -37,3 +67,16 @@ cron.schedule('* * * * *', () => {
     console.log('Running scheduled backup...');
     performBackup();
 });
+
+
+function getCurrentNumericDate() {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0'); 
+    const day = String(currentDate.getDate()).padStart(2, '0');
+    const hours = String(currentDate.getHours()).padStart(2, '0');
+    const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+    const seconds = String(currentDate.getSeconds()).padStart(2, '0');
+    
+    return `${year}${month}${day}${hours}${minutes}${seconds}`;
+}
--- server/dist/commonFunctions.js
+++ server/dist/commonFunctions.js
@@ -25,8 +25,35 @@ function performBackup() {
     const backupDir = path_1.default.resolve(__dirname, '..', 'backup.log');
     try {
         const data = fs_1.default.readFileSync(dataFilePath, 'utf8');
+        //backing up the data to the backup file
         fs_1.default.appendFileSync(backupDir, data, 'utf8');
-        console.log('Backup successful:', backupDir);
+        //creating a new backup file with current timestamp
+        const filePath = `../backups/backup-${getCurrentNumericDate()}.log`;
+        fs_1.default.writeFile(filePath, data, (err) => {
+            if (err) {
+                console.error('There was an error writing to the file:', err);
+            }
+            else {
+                console.log('File was written successfully');
+            }
+        });
+        const filePathEncrypted = `../encryptedBackup/backup-${getCurrentNumericDate()}.log`;
+        const algorithm = 'aes-256-cbc';
+        const secretKey = 'vOVH6sdmpNWjRRIqCc7rdxs01lwHzfr3';
+        const iv = crypto_1.default.randomBytes(16);
+        function encrypt(text) {
+            const cipher = crypto_1.default.createCipheriv(algorithm, secretKey, iv);
+            const encrypted = Buffer.concat([cipher.update(text), cipher.final()]);
+            return { iv: iv.toString('hex'), content: encrypted.toString('hex') };
+        }
+        fs_1.default.writeFile(filePathEncrypted, JSON.stringify(encrypt(data)), (err) => {
+            if (err) {
+                console.error('There was an error writing to the encrypted file:', err);
+            }
+            else {
+                console.log('Encrypted file was written successfully');
+            }
+        });
     }
     catch (error) {
         console.error('Error during backup:', error.message);
@@ -38,3 +65,13 @@ node_cron_1.default.schedule('* * * * *', () => {
     console.log('Running scheduled backup...');
     performBackup();
 });
+function getCurrentNumericDate() {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+    const day = String(currentDate.getDate()).padStart(2, '0');
+    const hours = String(currentDate.getHours()).padStart(2, '0');
+    const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+    const seconds = String(currentDate.getSeconds()).padStart(2, '0');
+    return `${year}${month}${day}${hours}${minutes}${seconds}`;
+}
